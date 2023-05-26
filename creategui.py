7aa0748d378119c78902d385fe8e275fe26f51ed
Bluefoux <Filip.HinderupMKK@gmail.com>
2023-05-26 11:59:13 +0200

    added things in create comp button clicked

--- creategui.py
+++ creategui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import sqlclass as sql
+import competition as comp
 
 """
 TODO:
@@ -10,20 +11,50 @@ program knapp
 Lägg till scroll ting för competitions
 """
 
-def Create_comp_button_clicked():
+
+def savecompbutton_clicked(db, mycursor):
+    compname = comp.competition()
+    compname.save(mycursor, db)
+    print("Save button was clicked!")
+
+def Create_comp_button_clicked(db, mycursor):
+    create_compwind = tk.Tk()
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Name", relief="ridge", anchor="w")
+    e.grid(row=1, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Start Date", relief="ridge", anchor="w")
+    e.grid(row=2, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "End Date", relief="ridge", anchor="w")
+    e.grid(row=3, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Venue", relief="ridge", anchor="w")
+    e.grid(row=4, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Oranizer", relief="ridge", anchor="w")
+    e.grid(row=5, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Number of lanes", relief="ridge", anchor="w")
+    e.grid(row=6, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Pool length", relief="ridge", anchor="w")
+    e.grid(row=7, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Individual fee", relief="ridge", anchor="w")
+    e.grid(row=8, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Relay fee", relief="ridge", anchor="w")
+    e.grid(row=9, column=0)
+    e = tk.Label(create_compwind, width=20, fg='blue', text = "Description", relief="ridge", anchor="w")
+    e.grid(row=10, column=0)
+    savecompbutton = tk.Button(create_compwind, text="Save", command=savecompbutton_clicked)
+    create_compwind.mainloop()
     print("Button was clicked!")
 
 def myui():
     wind = tk.Tk()
     #wind.geometry("800x600")
     # Code to add widgets will go here...
-    Create_compbutton = tk.Button(wind, text="Create new competition", command=Create_comp_button_clicked)
-    #Create_compbutton.pack()
-    Create_compbutton.grid(row=0, column=0)
-
     sqlclassobj = sql.sqlting()
     db = sqlclassobj.connect()
     mycursor = sqlclassobj.createcursor()
+
+    Create_compbutton = tk.Button(wind, text="Create new competition", command=Create_comp_button_clicked(db, mycursor))
+    #Create_compbutton.pack()
+    Create_compbutton.grid(row=0, column=0)
+    
     sqlclassobj.mycursor.execute("SELECT * FROM competition")
     myresult = sqlclassobj.mycursor.fetchall()
     i = 1
