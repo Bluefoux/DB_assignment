bd34c4c2b2bf6fc97ec3816579051721e06f4ef1
Bluefoux <Filip.HinderupMKK@gmail.com>
2023-05-26 12:58:43 +0200

    Create comp button now work+modified entrybox

--- creategui.py
+++ creategui.py
@@ -14,7 +14,7 @@ Lägg till scroll ting för competitions
 
 def savecompbutton_clicked(db, mycursor, mylst, description):
     compname = comp.competition(mylst[0].get(), mylst[1].get(), mylst[2].get(), mylst[3].get(), mylst[4].get(), mylst[5].get(), mylst[6].get(), mylst[7].get(), mylst[8].get(), description.get())
-    compname.save(mycursor, db)
+    #compname.save(mycursor, db)
     print("Save button was clicked!")
 
 def Create_comp_button_clicked(db, mycursor):
@@ -25,16 +25,16 @@ def Create_comp_button_clicked(db, mycursor):
     for x in mytup:
         e = tk.Label(create_compwind, width=20, fg='blue', text = x, relief="ridge", anchor="w")
         e.grid(row=i, column=0)
-        myentry = tk.Entry(create_compwind, bd = 10)
+        myentry = tk.Entry(create_compwind, bd = 5)
         myentry.grid(row=i, column=1)
         mylst.append(myentry)
         i += 1
     
     e = tk.Label(create_compwind, width=20, fg='blue', text = "Description", relief="ridge", anchor="w")
     e.grid(row=10, column=0)
-    description = tk.Entry(create_compwind, bd = 30)
-    e.grid(row=1, column=0)
-    savecompbutton = tk.Button(create_compwind, text="Save", command=savecompbutton_clicked(db, mycursor, mylst, description))
+    description = tk.Entry(create_compwind, bd = 5)
+    description.grid(row=10, column=1)
+    savecompbutton = tk.Button(create_compwind, text="Save", command= lambda: savecompbutton_clicked(db, mycursor, mylst, description))
     savecompbutton.grid(row=11, column=1)
     create_compwind.mainloop()
     print("Button was clicked!")
@@ -44,10 +44,10 @@ def myui():
     #wind.geometry("800x600")
     # Code to add widgets will go here...
     sqlclassobj = sql.sqlting()
-    db = sqlclassobj.connect()
-    mycursor = sqlclassobj.createcursor()
+    sqlclassobj.connect()
+    sqlclassobj.createcursor()
 
-    Create_compbutton = tk.Button(wind, text="Create new competition", command=Create_comp_button_clicked(db, mycursor))
+    Create_compbutton = tk.Button(wind, text="Create new competition", command= lambda: Create_comp_button_clicked(sqlclassobj.mydb, sqlclassobj.mycursor))
     #Create_compbutton.pack()
     Create_compbutton.grid(row=0, column=0)
     
